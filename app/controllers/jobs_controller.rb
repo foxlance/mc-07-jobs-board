c918101d8dbd7b329d070b81814d3b392dfec220
Lance Vincent <lancevincent@gmail.com>
2015-08-24 12:19:48 +0800

    completed CRUD for jobs, validation and association

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,62 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:edit, :show, :update, :destroy]
+
+  def index
+    if params[:category].blank?
+      @jobs = Job.all.order('created_at DESC')
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+
+      @jobs = Job.where(category_id: @category_id).order('created_at DESC')
+    end
+  end
+
+  def show
+    
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to @job, notice: "Job was successfully created!"
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    @job.update(job_params)
+
+    if @job.save
+      redirect_to @job, notice: "Job was successfully updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @job.destroy
+
+    redirect_to root_path, notice: "Job was successfully removed!"
+  end
+
+
+  private
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description, :company, :url, :category_id)
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,6 @@
 class Job < ActiveRecord::Base
+  belongs_to :category
+
+  validates :title, :company, :description, :url, :category_id, presence: :true
+  validates :title, :company, :description, :url, length: {minimum: 5}
 end
